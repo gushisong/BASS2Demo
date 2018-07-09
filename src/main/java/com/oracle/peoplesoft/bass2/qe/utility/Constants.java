aad0d52158aaaf64df25d26e3595ed1c95e8b0ad
xiaolei.song <xiaolei.song@oracle.com>
2018-07-09 16:17:14 +0800

    A

--- src/main/java/com/oracle/peoplesoft/bass2/qe/utility/Constants.java
+++ src/main/java/com/oracle/peoplesoft/bass2/qe/utility/Constants.java
@@ -1,6 +1,25 @@
 package com.oracle.peoplesoft.bass2.qe.utility;
 
+import java.io.File;
+import java.util.Properties;
+
 public class Constants {
+	
+	private static final String configFile=System.getProperty("user.dir")+File.separator+"config"+File.separator+"config.properties";
+	
+	private static PropertiesUtils prop=new PropertiesUtils(configFile);
+	
+//	public static final String BrowserPath=prop.getProperty("BrowserPath");
+	
+	public static final String URL=prop.getValueByKey("URL");
+	
+	
+	
+	
+	
+
+	
+	
 	
 	
 
