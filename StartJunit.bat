d03cca883df06ff2c3c6657d842229739fd6d426
xiaolei.song <xiaolei.song@oracle.com>
2018-07-10 15:38:22 +0800

    A

--- StartJunit.bat
+++ StartJunit.bat
@@ -1,3 +1,4 @@
 set CLASSPATH=D:\Java\Java_lib\junit-4.12.jar;D:\Java\Java_lib\selenium-server-standalone-3.13.0.jar;D:\Java\Java_lib\log4j-1.2.17.jar;C:\Users\xiaolson\git\BASS2Demo\build\libs\BASS2Demo.jar
 java com.oracle.peoplesoft.bass2.qe.testcase.BugAU_Test 
 
+
